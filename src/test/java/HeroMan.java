8683f4334b34a6de81e6bcb96bb2b803c12a2728
Mehary16 <mehary.aynealem@gmail.com>
2020-11-01 14:16:03 -0600

    Womans are the light the house

--- src/test/java/HeroMan.java
+++ src/test/java/HeroMan.java
@@ -4,5 +4,7 @@ public class HeroMan {
 
         System.out.println("Hello batch 20 Group 14 you guys are so awesome, you were head of this git class");
         System.out.println("Thank you Sergio for creating this");
+
+        System.out.println("This is dedicate for our group ladies which are the shing stars of the group");
     }
 }
